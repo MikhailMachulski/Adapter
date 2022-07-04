1a3da78bf3f5215e26fa109b50ec5a135a2d078c
Mikhail Machulski <mik.machulski@gmail.com>
2022-07-04 15:34:35 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(7, 10));
+        System.out.println(calc.sum(71, 9));
+        System.out.println(calc.pow(10, 2));
+    }
+}
